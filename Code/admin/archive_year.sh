60ef08f6672b565f2146f416b935558b68c7f547
David Rosenberg <david.davidr@gmail.com>
2017-09-21 17:23:34 -0400

    Added ConceptChecks dir to things archived

--- Code/admin/archive_year.sh
+++ Code/admin/archive_year.sh
@@ -4,9 +4,9 @@
 cd ~/Dropbox/repos/mlcourse
 FLATTEN="python Code/admin/flatten_lyx.py"
 LYX2LYX="python /Applications/LyX.app/Contents/Resources/lyx2lyx/lyx2lyx"
-YEAR=2016
+YEAR=2017
 ORIGIN="." 
-ORIGIN="2015" 
+##ORIGIN="2017" 
 DEST="Archive"/$YEAR
 rm -rf $DEST
 ## Flatten lectures
@@ -35,6 +35,11 @@ rsync -av $ORIGIN/Labs $DEST/
 ## Copy Code
 rsync -av Code $DEST/
 rm -rf $DEST/Code/admin
+
+## Copy Concept Checks
+rsync -av ConceptChecks $DEST/
+
+
 
 ## Convert to newest LyX version
 ##find $DEST/Lectures -name "*.lyx" |  parallel --verbose 'echo cp {} {//}/old; '
